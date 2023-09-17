2bfdf5776a1e82cef65a8677adc92ef3ed1c3681
Artuhinv <vitalik.artuhin@gmail.com>
2023-09-17 23:46:17 +0300

    first commit

---
+++ src/test/java/com/example/Less/LessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Less;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
